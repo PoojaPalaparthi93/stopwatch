edee713c3200a2c0b60e81ec549cb1f159f99026
PoojaPalaparthi93 <poojapalaparthi2016@gmail.com>
2024-06-26 22:54:48 +0530

    Stopwatch creation

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,10 +1,27 @@
+import './App.css';
+import { useState } from "react"
+
 
 function App() {
+
+  const [counter, setCounter] = useState(0)
+  const [running, setRunning] = useState(false)
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <h1>Stopwatch Counter App</h1>
+        <h1>Stopwatch App</h1>
       </header>
+      <div className='counterText'>
+        <span>{("0" + Math.floor((counter / 60000) % 60 ))}</span>
+        <span>{("0" + Math.floor((counter / 1000) % 60 ))}</span>
+        <span>{("0" + Math.floor((counter / 10) % 100 ))}</span>
+      </div>
+      <div className="btnContainer">
+        <button onClick={() => { setRunning(true) }}> Start </button>
+        <button onClick={() => { setRunning(false) }}> Pause </button>
+        <button onClick={() => { setCounter(0) }}> Reset </button>
+      </div>
     </div>
   );
 }
