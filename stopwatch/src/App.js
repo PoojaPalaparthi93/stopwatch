9d3e3671681ffbb11a7173352acbe70172a33152
PoojaPalaparthi93 <poojapalaparthi2016@gmail.com>
2024-06-26 21:52:03 +0530

    Update

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Stopwatch Counter</h1>
+        <h1>Stopwatch Counter App</h1>
       </header>
     </div>
   );
